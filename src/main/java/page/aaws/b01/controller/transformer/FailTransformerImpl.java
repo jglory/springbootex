488980fda9298a24f3b16bd3c901eb4eea9fdc3e
yongmoon jo <yongmoon@hanmail.net>
2023-10-08 18:10:08 +0900

    [main] FailTransformer 클래스를 stateless 하게 수정

--- src/main/java/page/aaws/b01/controller/transformer/FailTransformerImpl.java
+++ src/main/java/page/aaws/b01/controller/transformer/FailTransformerImpl.java
@@ -1,13 +1,4 @@
 package page.aaws.b01.controller.transformer;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-import org.springframework.http.HttpStatusCode;
-
-@AllArgsConstructor
-@Getter
 public abstract class FailTransformerImpl implements Transformer {
-    private final HttpStatusCode httpStatusCode;
-    private final Exception exception;
 }
