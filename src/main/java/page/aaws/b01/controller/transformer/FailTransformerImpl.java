eadf4520625bc0b679c130a493c6e6929e0dc559
yongmoon jo <yongmoon@hanmail.net>
2023-10-08 18:05:08 +0900

    [main] 코드 정리

--- src/main/java/page/aaws/b01/controller/transformer/FailTransformerImpl.java
+++ src/main/java/page/aaws/b01/controller/transformer/FailTransformerImpl.java
@@ -1,17 +1,14 @@
 package page.aaws.b01.controller.transformer;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import org.springframework.http.HttpStatusCode;
 import page.aaws.b01.transformer.Transformer;
 
+@AllArgsConstructor
 @Getter
 public abstract class FailTransformerImpl implements Transformer {
-    private HttpStatusCode httpStatusCode;
-    private Exception exception;
-
-    public FailTransformerImpl(HttpStatusCode httpStatusCode, Exception exception) {
-        this.httpStatusCode = httpStatusCode;
-        this.exception = exception;
-    }
+    private final HttpStatusCode httpStatusCode;
+    private final Exception exception;
 }
--- src/main/java/page/aaws/b01/transformer/Transformer.java
+++ src/main/java/page/aaws/b01/transformer/Transformer.java
@@ -3,5 +3,5 @@ package page.aaws.b01.transformer;
 import org.springframework.http.ResponseEntity;
 
 public interface Transformer {
-    public ResponseEntity<?> process();
+    ResponseEntity<?> process();
 }
