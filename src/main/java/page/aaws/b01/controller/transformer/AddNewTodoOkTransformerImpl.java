fc033ac332e7ff387ba7c2c52e8bc00c819a6f70
yongmoon jo <yongmoon@hanmail.net>
2023-10-23 18:00:41 +0900

    [main] AddNewTodoTransformer bean 형태로 변경

--- src/main/java/page/aaws/b01/controller/transformer/AddNewTodoOkTransformerImpl.java
+++ src/main/java/page/aaws/b01/controller/transformer/AddNewTodoOkTransformerImpl.java
@@ -1,8 +1,10 @@
 package page.aaws.b01.controller.transformer;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import page.aaws.b01.dto.TodoDto;
 
+@Component
 public class AddNewTodoOkTransformerImpl extends AddNewTodoOkTransformer {
     @Override
     public ResponseEntity<?> process(Object... data) {
